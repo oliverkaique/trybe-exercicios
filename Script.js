c1bedf242ad570f0a9c7b3f0d123e5047e699eff
oliverkaique <kaiquekurosaki@hotmail.com>
2023-01-25 10:25:44 -0300

    realizando operações simples

--- Script.js
+++ Script.js
@@ -1,2 +1,14 @@
+// declarando constantes com seus
 const a = 10;
 const b = 30;
+
+let somadosvalores = a + b;
+console.log(somadosvalores);
+let subdosvalores = a - b;
+console.log(subdosvalores);
+let multdosvalores = a * b;
+console.log(multdosvalores);
+let divdosvalores = a / b;
+console.log(divdosvalores);
+let restodosvalores = a % b;
+console.log(restodosvalores);
